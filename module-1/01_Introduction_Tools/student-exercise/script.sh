877dadaa85aec2c06728cd49181c2e8f981021ad
Sean Ryan <sryan488@gmail.com>
2019-09-16 17:15:22 -0400

    Submitting Week 1 Day 1 exercise

--- module-1/01_Introduction_Tools/student-exercise/script.sh
+++ module-1/01_Introduction_Tools/student-exercise/script.sh
@@ -30,52 +30,75 @@ cd ../canada
 mkdir quebec
 
 # create a new directory called 'british columbia'
-mkdir 'british columbia' 
+mkdir 'british columbia'
 
 # change your working directory to the 'ohio' directory
+cd ../usa/ohio
 
 # create a new directory called 'cuyahoga'
+mkdir cuyahoga
 
 # create a new directory called 'hamilton'
+mkdir hamilton
 
 # create a new directory called 'franklin'
+mkdir franklin
 
 # change your working directory to the 'pennsylvania' directory
+cd ../pennsylvania
 
 # create a new directory called 'allegheny'
+mkdir allegheny
 
 # change your working directory to the 'michigan' directory
+cd ../michigan
 
 # create a new directory called 'wayne'
+mkdir wayne
 
 # change your working directory to the 'cuyahoga' directory
+cd ../ohio/cuyahoga
 
 # create a new file in the working directory called 'cleveland.txt'
+touch cleveland.txt
 
 # create a new file in the working directory called 'cincinnati.txt'
+touch cincinnati.txt
 
 # change your working directory to the 'hamilton' directory
+cd ../hamilton
 
 # move the file called 'cincinnati.txt' from the 'cuyahoga' directory
 # to the 'hamilton' directory
+mv ../cuyahoga/cincinnati.txt .
 
 # copy the file from the 'hamilton' directory called 'cincinnati.txt' into 
 # the 'franklin' directory and call it 'columbus.txt'
+cp cincinnati.txt ../franklin/columbus.txt
 
 # change your working directory to the 'allegheny' directory
+cd ../../pennsylvania/allegheny
 
 # create a new file in the working directory called 'pittsburgh.txt'
+touch pittsburgh.txt
 
 # create a new file in the wayne directory called 'detroit.txt'
+touch ~/playground/usa/michigan/wayne/detroit.txt
 
 # change your working directory to the 'quebec' directory
+cd ~/playground/canada/quebec
 
 # create a new file in the working directory called 'montreal.txt'
+touch montreal.txt
 
 # create a new file in the working directory called 'quebec-city.txt'
+touch quebec-city.txt 
 
 # change your working directory to the 'british columbia' directory
+cd ~/playground/canada/'british columbia'
 
 # create a new file in the working directory called 'vancouver.txt'
+touch vancouver.txt
 
 # create a new file in the working directory called 'prince-george.txt'
+touch prince-george.txt

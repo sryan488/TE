f4d19c83b41f54197c8ad716ede209acbc7f4729
Mike Morel <michael.morel@techelevator.com>
2019-09-17 21:12:15 -0400

    M1D3 lecture

---
+++ module-1/03_Expressions/student-lecture/Lecture/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Declare a variable
+
+            // Start a statement block
+
+            // Print out the variable defined in the outer scope
+
+            // Declare a variable in the block (inner scope)
+
+            // Print out that variable
+
+            // End the statement block
+
+            // Print the the variable declared in the block
+
+
+
+            // Create and call a method
+
+
+            // Create and print some boolean expressions
+
+
+            Console.ReadKey();
+        }
+    }
+}

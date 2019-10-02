e5aedbbf0a1e8227553fb842f3bb14d2e2783ac3
Mike Morel <michael.morel@techelevator.com>
2019-10-01 21:18:43 -0400

    M1D13 student

---
+++ module-1/13_Inheritance_Abstract_Classes/student-lecture/OOPDemo/OOPDemo/Models/ChildC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPDemo.Models
+{
+    public class ChildC : ParentB
+    {
+        public ChildC(string shoeColor, int shoeSize) : base(shoeColor)
+        {
+            Console.WriteLine($"ChildC constructor: {shoeColor}, {shoeSize}");
+        }
+
+//        public override string PublicDerivedStringA => base.PublicDerivedStringA;
+        public override string PublicDerivedStringA
+        {
+            get
+            {
+                return "ChildC.PublicDerivedStringA";
+            }
+        }
+    }
+}
---
+++ module-1/13_Inheritance_Abstract_Classes/student-lecture/Shapes/Shapes/Models/Text.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes.Models
+{
+    class Text : IDrawable
+    {
+        public string Label { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public Text(string label, ConsoleColor color)
+        {
+            Label = label;
+            Color = color;
+        }
+
+        public void Draw()
+        {
+            ConsoleColor savedColor = Console.ForegroundColor;
+            Console.ForegroundColor = Color;
+            Console.WriteLine(Label);
+            Console.ForegroundColor = savedColor;
+
+        }
+
+        public override string ToString()
+        {
+            return $"A {Color} Text Label";
+        }
+
+    }
+}

bcfcd77f35a6d98cf9a1eedb403d9768090baf5f
Mike Morel <michael.morel@techelevator.com>
2019-10-02 13:54:23 -0400

    M1D13 lecture

---
+++ module-1/13_Inheritance_Abstract_Classes/lecture-final/OOPDemo/OOPDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using OOPDemo.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OOPDemo
+{
+    class Program
+    {
+        static List<Person> people = new List<Person>()
+        {
+            new Person("Barney", "Rubble", 1950, 12, 12, 5000),
+            new Person("Betty", "Rubble", 1954, 1, 8, 30000),
+            new Person("Fred", "Flintstone", 1948, 3, 15, 32000),
+            new Person("Wilma", "Flintstone", 1955, 11, 10, 8000),
+
+        };
+
+        /// <summary>
+        /// Purpose of this program is to demonstrate the OO concepts and keywords we have used so far
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            DisplayPeople();
+            Console.ReadLine();
+            return;
+
+            // Create a gp, parent and child. Follow the contructors
+            Console.WriteLine("Creating a grandparent");
+            GrandparentA gp = new GrandparentA();
+            Console.WriteLine($"GP: {gp.PublicDerivedStringA} ");
+            Console.WriteLine(gp.VirtualMethodA2("Foo"));
+
+
+            Console.WriteLine(  "*****************************");
+            Console.WriteLine("Creating a parent");
+            ParentB p = new ParentB("Red");
+            Console.WriteLine($"P: {p.PublicDerivedStringA}");
+            Console.WriteLine(p.VirtualMethodA2("Bar"));
+
+
+            Console.WriteLine("*****************************");
+            Console.WriteLine("Creating a child");
+            ChildC c = new ChildC("Brown", 11);
+            Console.WriteLine($"C: {c.PublicDerivedStringA}");
+            Console.WriteLine(c.VirtualMethodA2("Groovy"));
+
+
+            // Get the Derived property on each and display it.
+
+
+            // Execute the virtual method on each and diplay the result
+
+            Console.ReadLine();
+        }
+
+        public static void DisplayPeople()
+        {
+            foreach (Person p in people)
+            {
+                Console.WriteLine($"{p,-30} {p.BirthDate,-10:d} {p.Age,4} {p.Income,15:C}");
+            }
+        }
+    }
+}
---
+++ module-1/13_Inheritance_Abstract_Classes/lecture-final/Shapes/Shapes/Models/Shape2D.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes.Models
+{
+    /// <summary>
+    /// A two-dimensional shape the can be drawn on the screen
+    /// </summary>
+    abstract public class Shape2D : IDrawable
+    {
+        public bool IsFilled { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        abstract public int Area { get; }
+
+        abstract public int Perimeter { get; }
+
+        abstract public void Draw();
+
+
+        public override string ToString()
+        {
+            return $"A {Color} shape that is{(IsFilled ? "" : " not")} filled.";
+        }
+
+        #region Helper Methods
+        // A place to save the current color for restoring after the draw
+        private ConsoleColor savedColor = ConsoleColor.White;
+
+        protected void SetConsoleColor()
+        {
+            this.savedColor = Console.ForegroundColor;
+            Console.ForegroundColor = Color;
+        }
+
+        protected void ResetConsoleColor()
+        {
+            Console.ForegroundColor = savedColor;
+        }
+        #endregion
+
+
+    }
+}

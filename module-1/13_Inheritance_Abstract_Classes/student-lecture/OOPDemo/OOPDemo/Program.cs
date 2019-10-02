d6624ca3e3457b82a3474380d4a377bad19eaf6d
Mike Morel <michael.morel@techelevator.com>
2019-10-02 08:45:46 -0400

    Added person display

--- module-1/13_Inheritance_Abstract_Classes/student-lecture/OOPDemo/OOPDemo/Program.cs
+++ module-1/13_Inheritance_Abstract_Classes/student-lecture/OOPDemo/OOPDemo/Program.cs
@@ -1,16 +1,30 @@
 ﻿using OOPDemo.Models;
 using System;
+using System.Collections.Generic;
 
 namespace OOPDemo
 {
     class Program
     {
+        static List<Person> people = new List<Person>()
+        {
+            new Person("Barney", "Rubble", 1950, 12, 12, 5000),
+            new Person("Betty", "Rubble", 1954, 1, 8, 30000),
+            new Person("Fred", "Flintstone", 1948, 3, 15, 32000),
+            new Person("Wilma", "Flintstone", 1955, 11, 10, 8000),
+
+        };
+
         /// <summary>
         /// Purpose of this program is to demonstrate the OO concepts and keywords we have used so far
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            DisplayPeople();
+            Console.ReadLine();
+            return;
+
             // Create a gp, parent and child. Follow the contructors
             Console.WriteLine("Creating a grandparent");
             GrandparentA gp = new GrandparentA();
@@ -38,6 +52,14 @@ namespace OOPDemo
             // Execute the virtual method on each and diplay the result
 
             Console.ReadLine();
+        }
+
+        public static void DisplayPeople()
+        {
+            foreach (Person p in people)
+            {
+                Console.WriteLine($"{p.LastName} {p.FirstName} {p.BirthDate} {p.Age} {p.Income}");
+            }
         }
     }
 }

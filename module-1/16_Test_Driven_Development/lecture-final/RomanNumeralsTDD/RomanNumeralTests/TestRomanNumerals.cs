e8b548a57c92879df4d5bfa4a1b434923b31c85e
Mike Morel <michael.morel@techelevator.com>
2019-10-07 11:04:39 -0400

    Done with step 3 of TDD exercise

---
+++ module-1/16_Test_Driven_Development/lecture-final/RomanNumeralsTDD/RomanNumeralTests/TestRomanNumerals.cs
@@ -0,0 +1,65 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumeralsTDD;
+
+namespace RomanNumeralTests
+{
+    [TestClass]
+    public class TestRomanNumerals
+    {
+        [DataTestMethod]
+        [DataRow(1, "I")]
+        [DataRow(5, "V")]
+        [DataRow(10, "X")]
+        [DataRow(50, "L")]
+        [DataRow(100, "C")]
+        [DataRow(500, "D")]
+        [DataRow(1000, "M")]
+        [DataRow(2, "II")]
+        public void TestRomanNumeral(int numberToTest, string expectedResult)
+        {
+            // Arrange
+            // Create a new RomanNumeral object
+            RomanNumeral roman = new RomanNumeral();
+
+            // Act
+            string actualRN = roman.ToRoman(numberToTest);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualRN);
+
+        }
+
+        //[TestMethod]
+        //public void TestRomanNumeralForOne()
+        //{
+        //    // Arrange
+        //    // Create a new RomanNumeral object
+        //    RomanNumeral roman = new RomanNumeral();
+
+        //    // Act
+        //    string actualRN = roman.ToRoman(1);
+
+
+        //    // Assert
+        //    Assert.AreEqual("I", actualRN);
+        
+        //}
+
+        //[TestMethod]
+        //public void TestRomanNumeralForFive()
+        //{
+        //    // Arrange
+        //    // Create a new RomanNumeral object
+        //    RomanNumeral roman = new RomanNumeral();
+
+        //    // Act
+        //    string actualRN = roman.ToRoman(5);
+
+
+        //    // Assert
+        //    Assert.AreEqual("V", actualRN);
+
+        //}
+
+    }
+}
---
+++ module-1/16_Test_Driven_Development/lecture-final/RomanNumeralsTDD/RomanNumeralsTDD/RomanNumeral.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace RomanNumeralsTDD
+{
+    public class RomanNumeral
+    {
+        public string ToRoman(int number)
+        {
+            #region Handle multiple (2 or 3) single-character numbers
+
+            switch (number)
+            {
+                case 1:
+                    return "I";
+                case 5:
+                    return "V";
+                case 10:
+                    return "X";
+                case 50:
+                    return "L";
+                case 100:
+                    return "C";
+                case 500:
+                    return "D";
+                case 1000:
+                    return "M";
+                default:
+                    return "";
+            }
+            #endregion
+
+            #region Satisfy all single-character numbers
+
+            //switch (number)
+            //{
+            //    case 1:
+            //        return "I";
+            //    case 5:
+            //        return "V";
+            //    case 10:
+            //        return "X";
+            //    case 50:
+            //        return "L";
+            //    case 100:
+            //        return "C";
+            //    case 500:
+            //        return "D";
+            //    case 1000:
+            //        return "M";
+            //    default:
+            //        return "";
+            //}
+            #endregion
+
+            #region Satisfy 1 and 5
+            //if (number == 5)
+            //{
+            //    return "V";
+            //}
+            //return "I";
+            #endregion
+
+            #region V1: Return value for 1
+            //return "I";
+            #endregion
+
+        }
+    }
+}

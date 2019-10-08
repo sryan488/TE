204b83e60595f598e261b25e6ae5ac173ead28ac
Mike Morel <michael.morel@techelevator.com>
2019-10-08 08:43:09 -0400

    M1D17 student

---
+++ module-1/17_FileIO_Reading_in/student-lecture/FileIO_Exceptions_lecture/FileAndDirectory/Classes/Navigator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FileAndDirectory.Classes
+{
+    class Navigator
+    {
+        public void Run()
+        {
+            while (true)
+            {
+                string currentPath = Directory.GetCurrentDirectory();
+                Console.Clear();
+
+                Console.WriteLine("Windows Navigator");
+                Console.WriteLine("-----------------");
+                Console.WriteLine($"Current Directory: {currentPath}");
+                Console.WriteLine();
+                Console.WriteLine("1 - Navigate Directories");
+                Console.WriteLine("2 - List Files");
+                Console.WriteLine("3 - Read a file");
+                Console.WriteLine("Q - Quit Navigator");
+
+                switch (Console.ReadLine().Trim().ToLower())
+                {
+                    case "q":
+                        return;
+                    case "1":
+                        NavigateDirectories();
+                        break;
+                    case "2":
+                        ListFiles();
+                        break;
+                    case "3":
+                        ReadFile();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice, please try again.");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+
+        private void NavigateDirectories()
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                // Get current and root directories here...
+                string currentPath = "????";
+                string root = "??";
+                bool hasParent = (currentPath != root);
+                Console.WriteLine("Navigate Directories Submenu");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine($"Current Directory: {currentPath}");
+                Console.WriteLine();
+
+                /// Get the list of dirs here...
+                string[] dirs = new string[] { };
+
+
+                if (hasParent)
+                {
+                    Console.WriteLine("0 - UP to Parent");
+                }
+                for (int i = 0; i < dirs.Length; i++)
+                {
+
+                    // Get the relative path here...
+                    string relativePath = "????";
+                    Console.WriteLine($"{i + 1} - {relativePath}");
+
+                }
+                Console.WriteLine("Q - Back to Navigator menu");
+
+                string input = Console.ReadLine().Trim().ToLower();
+                if (input == "q")
+                {
+                    break;
+                }
+                else if (input == "0" && hasParent)
+                {
+                    // Calculate parent folder here, and move there...
+
+
+
+                }
+                else
+                {
+                    // Navigate to the folder at [selection - 1] here....
+                        Console.WriteLine("Invalid selection. Please try again.");
+                        Console.ReadLine();
+                }
+            }
+        }
+
+        private void ListFiles()
+        {
+            // List all the files in the current directory
+            Console.Clear();
+
+            // Get the current directory here...
+            string currentPath = "????";
+            Console.WriteLine("List Files Submenu");
+            Console.WriteLine("------------------");
+            Console.WriteLine($"Current Directory: {currentPath}");
+            Console.WriteLine();
+
+            // List all the files here....
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Press RETURN to continue");
+            Console.ReadLine();
+        }
+
+        private void ReadFile()
+        {
+            // List the files in the current dir and allow the user to select one.
+
+        }
+
+
+    }
+}
---
+++ module-1/17_FileIO_Reading_in/student-lecture/FileIO_Exceptions_lecture/States/Model/State.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace States.Model
+{
+    class State
+    {
+        public string StateCode { get; set; }
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public string LargestCity { get; set; }
+
+        public State(string stateCode, string name, string capital, string largestCity)
+        {
+            this.StateCode = stateCode;
+            this.Name = name;
+            this.Capital = capital;
+            this.LargestCity = largestCity;
+        }
+
+    }
+}

327ca489fe88eda82b672aa0112dec2b76d06e17
Sean Ryan <sryan488@gmail.com>
2019-10-14 12:11:24 -0400

    Assessment

--- module-1/18_FileIO_Writing_out/student-exercise/FizzWriter/Program.cs
+++ module-1/18_FileIO_Writing_out/student-exercise/FizzWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FizzWriter
 {
@@ -6,7 +7,44 @@ namespace FizzWriter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Directory.SetCurrentDirectory(@"../../../../");
+
+            string outPath = "..\\..\\..\\FizzBuzz.txt";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(outPath, true))
+
+                {
+                    for (int i = 0; i <= 300; i++)
+                    {
+
+                        if (i % 15 == 0)
+                        {
+                            sw.WriteLine("FizzBuzz");
+                        }
+                        else if (i % 3 == 0)
+                        {
+                            sw.WriteLine("Fizz");
+                        }
+                        else if (i % 5 == 0)
+                        {
+                            sw.WriteLine("Buzz");
+                        }
+                        else 
+                        {
+                            sw.WriteLine(i);
+                        }
+
+
+                    }
+                }
+            }
+            catch(Exception x)
+            {
+                Console.WriteLine($"There was an ERROR: {x.Message}");
+            }
+
         }
     }
 }
---
+++ module-1/Module_1_Coding_Assessment/Mod1Assessment/Mod1Assessment/FlowerShopOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod1Assessment
+{
+    public class FlowerShopOrder
+    {
+        string BouquetType { get; set; }
+        int NumberOfRoses { get; set; }
+        decimal Subtotal { get; set; }
+
+
+        public FlowerShopOrder(string bouquetType, int numberOfRoses)
+        {
+            this.BouquetType = bouquetType;
+            this.NumberOfRoses = numberOfRoses;
+        }
+
+        public 
+
+
+    }
+
+
+}

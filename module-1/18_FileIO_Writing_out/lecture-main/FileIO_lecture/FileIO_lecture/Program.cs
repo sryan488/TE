93acd85c683b26fbc8db91def9b8ec6bc35bed3a
Mike Morel <michael.morel@techelevator.com>
2019-10-14 08:57:52 -0400

    M1D18 lecture, M2D1 student plus M1 coding assessment

---
+++ module-1/18_FileIO_Writing_out/lecture-main/FileIO_lecture/FileIO_lecture/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace FileIO_lecture
+{
+    
+    //class A
+    //{
+
+    //}
+
+    //class B : A
+    //{
+
+    //}
+
+    //class C : A
+    //{
+
+    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output will go to this file
+            string outPath = "..\\..\\..\\Folders.txt";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(outPath, true))
+                {
+                    string spelunkingPath = "c:\\git";
+
+                    string[] dirs1 = Directory.GetDirectories(spelunkingPath);
+
+                    sw.WriteLine($"Subfolders of {spelunkingPath} at {DateTime.UtcNow:f}:");
+
+                    foreach (string subfolder in dirs1)
+                    {
+                        sw.WriteLine($"\t{subfolder}");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                // Tell the user there is a problem
+                Console.WriteLine($"There was an ERROR: {ex.Message}");
+            }
+
+
+
+            Console.WriteLine("Press RETURN");
+            Console.ReadKey();
+        }
+    }
+}

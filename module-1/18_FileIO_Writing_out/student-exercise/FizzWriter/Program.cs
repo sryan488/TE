af55733fc79343cbb39d7f6d238ac7df7ee71bb9
Sean Ryan <sryan488@gmail.com>
2019-10-14 14:51:10 -0400

    FizzWriter, late commit for 2pts max

--- module-1/18_FileIO_Writing_out/student-exercise/FizzWriter/Program.cs
+++ module-1/18_FileIO_Writing_out/student-exercise/FizzWriter/Program.cs
@@ -7,27 +7,27 @@ namespace FizzWriter
     {
         static void Main(string[] args)
         {
-            Directory.SetCurrentDirectory(@"../../../../");
 
-            string outPath = "..\\..\\..\\FizzBuzz.txt";
+
+            string outPath = "..\\..\\..\\..\\FizzBuzz.txt";
 
             try
             {
                 using (StreamWriter sw = new StreamWriter(outPath, true))
 
                 {
-                    for (int i = 0; i <= 300; i++)
+                    for (int i = 1; i <= 300; i++)
                     {
 
-                        if (i % 15 == 0)
+                        if (i % 15 == 0 || i.ToString().Contains("3") && i.ToString().Contains("5"))
                         {
                             sw.WriteLine("FizzBuzz");
                         }
-                        else if (i % 3 == 0)
+                        else if (i % 3 == 0 || i.ToString().Contains("3"))
                         {
                             sw.WriteLine("Fizz");
                         }
-                        else if (i % 5 == 0)
+                        else if (i % 5 == 0 || i.ToString().Contains("5"))
                         {
                             sw.WriteLine("Buzz");
                         }

14d290aa479052eb70c23456d1617aff56d99f02
Mike Morel <michael.morel@techelevator.com>
2019-10-18 15:28:35 -0400

    M2D5 lecture

---
+++ module-2/05_Database_Design/lecture-final/ArtGalleryDB/ArtGalleryDB/Tables/Art.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Art]
+(
+    [ArtId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(50) NOT NULL DEFAULT 'Untitled', 
+    [ArtistId] INT NOT NULL, 
+    [InStoc] BIT NOT NULL DEFAULT 1, 
+    CONSTRAINT [FK_Art_Person] FOREIGN KEY (ArtistId) REFERENCES Person(PersonId)
+)
---
+++ module-2/05_Database_Design/lecture-final/ArtGalleryDB/ArtGalleryDB/Tables/Person.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+    [PersonId] INT NOT NULL IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Address] NVARCHAR(100) NULL, 
+    [Phone] VARCHAR(20) NULL, 
+    CONSTRAINT [PK_Person] PRIMARY KEY (PersonId)
+)

acd826b2d5cd9a9a8ee6dbb2d4200cad4a4390c7
Mike Morel <michael.morel@techelevator.com>
2019-10-31 08:52:07 -0400

    M3D3 lecture, M3D4 student

---
+++ module-3/03-Introduction-to-MVC-Views/lecture-final/TechElevator.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechElevator.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // TODO 01: Modify the Home/Index view 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // TODO 02: Add a Greeting Action to this controller and its related Home/Greeting view
+        public IActionResult Greeting()
+        {
+            return View();
+        }
+
+
+        // TODO 04: Add a Calculators Controller  http://localhost/Calculators/
+        // TODO 05: Add an Index view as the default page. Calculate MPH for all KPH from 0 to 100 KPH
+        // TODO 06: Add a KPH parameter to the action, and pass it to the view. Use that value to cap the loop.
+    }
+}

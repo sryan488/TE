d2b49fbbb1a94b58ec4701947d32d472a1eaed5f
Mike Morel <michael.morel@techelevator.com>
2019-10-31 22:22:21 -0400

    M3D4 lecture and M3D5 student

---
+++ module-3/04-MVC-Views-Part-2/lecture-final/student-exercise-instructor/MVCModels.Web/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCModels.Web.DAL;
+using MVCModels.Web.Models;
+
+namespace MVCModels.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductDAO productDAO;
+        public ProductsController(IProductDAO productDAO)
+        {
+            this.productDAO = productDAO;
+        }
+
+        // GET: products/
+        // GET: products/index
+        // GET: products/index?minPrice=25&sortOrder=PriceHighToLow
+        public IActionResult Index(ProductFilter filter, ProductSortOrder sortOrder = ProductSortOrder.Default)
+        {
+            IList<Product> products = productDAO.GetAll(filter, sortOrder);
+            return View(products);
+        }
+
+        // GET: products/tile?minPrice=50
+        // GET: products/tile
+        public IActionResult Tile(ProductFilter filter)
+        {
+            IList<Product> products = productDAO.GetAll(filter, ProductSortOrder.Default);
+            string message = $"{products.Count} {(products.Count == 1 ? "product" : "products")} were found.";
+
+            // TODO: Add the message to ViewData, then show it in the view
+            return View(products);
+        }
+
+        // GET: products/detail/1
+        // GET: products/detail?id=1
+        public IActionResult Detail(int id)
+        {
+            Product product = productDAO.GetById(id);
+            return View(product);
+        }
+    }
+}
---
+++ module-3/05-MVC-Views-Part-3/student-lecture/TechElevator.Web/Controllers/FilmsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TechElevator.Web.DAL;
+using TechElevator.Web.Models;
+
+namespace TechElevator.Web.Controllers
+{
+    public class FilmsController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Get a list of all Film
+            MockStarWarsDAO dao = new MockStarWarsDAO();
+            IList<Film> films = dao.GetFilms();
+
+            // Pass the list of films into the View
+            ViewResult viewResult = View(films);
+            return viewResult;
+        }
+
+        public IActionResult Details(string id)
+        {
+            // Create a dao to access the "database"
+            MockStarWarsDAO dao = new MockStarWarsDAO();
+
+            // Call the method to get a single film
+            Film film = dao.GetFilm(id);
+
+            // Pass the film into the Details view
+            return View(film);
+        }
+    }
+}

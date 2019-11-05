9d811124dce4d276a118e638ef79af63ffdbe62d
Mike Morel <michael.morel@techelevator.com>
2019-11-05 13:02:58 -0500

    M3D7 lecture

---
+++ module-3/07-Forms-and-Controllers-HTTP-POST/lecture-final/Forms.Web/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Forms.Web.Models;
+using Forms.Web.DAL;
+
+namespace Forms.Web.Controllers
+{
+    public class HomeController : Controller
+    {        
+        /// <summary>
+        /// Represents an index action.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+            // TODO: Re-direct Home page to City Search or City Index
+        }   
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ module-3/07-Forms-and-Controllers-HTTP-POST/lecture-final/Forms.Web/Models/CitySearchVM.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forms.Web.Models
+{
+    public class CitySearchVM
+    {
+        // TODO 02: Data Labels: Use Data Annotations to set the display name so they show in the view
+        [Display(Name = "Country", Description = "Enter a country code")]
+        public string CountryCode { get; set; }
+
+        [Display(Name = "District (e.g., State)")]
+        public string District { get; set; }
+        public IList<City> Cities { get; set; }
+
+        // TODO 03: Add a SelectList for the Country List dropdown to the VM
+        public SelectList CountryList { get; set; }
+    }
+}

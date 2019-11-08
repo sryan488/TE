7372ab42bf88f1e61429a638b3ba8b8389844a7c
Mike Morel <michael.morel@techelevator.com>
2019-11-08 14:44:29 -0500

    M3D10 lecture

---
+++ module-3/10-Review/lecture-final/Recipes/Recipes/Controllers/RecipesBaseController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Recipes.Controllers
+{
+    public abstract class RecipesBaseController : Controller
+    {
+        private const string cuisineSessionKey = "PreferredCuisine";
+        protected string GetPreferredCuisine()
+        {
+            return HttpContext.Session.GetString(cuisineSessionKey) ?? "";
+        }
+
+        protected void SetPreferredCuisine(string cuisine)
+        {
+            if (cuisine == null || cuisine.Trim().Length == 0)
+            {
+                ClearPreferredCuisine();
+            }
+            else
+            {
+                HttpContext.Session.SetString(cuisineSessionKey, cuisine);
+            }
+        }
+
+        protected void ClearPreferredCuisine()
+        {
+            HttpContext.Session.Remove(cuisineSessionKey);
+        }
+
+        protected void SetMessage(string message)
+        {
+            TempData["message"] = message;
+        }
+    }
+}

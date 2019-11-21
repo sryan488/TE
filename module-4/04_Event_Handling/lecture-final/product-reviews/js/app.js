46d44731bf3045fa8525d47f25b80a49326e7e08
Mike Morel <michael.morel@techelevator.com>
2019-11-21 13:31:04 -0500

    M4D4 lecture, M4D5 tutorial

---
+++ module-4/04_Event_Handling/lecture-final/product-reviews/js/app.js
@@ -0,0 +1,193 @@
+const name = 'Cigar Parties for Dummies';
+let description = 'Host and plan the perfect cigar party for all of your squirrelly friends.';
+let reviews = [
+  {
+    reviewer: 'Malcolm Gladwell',
+    title: 'What a book!',
+    review:
+      "It certainly is a book. I mean, I can see that. Pages kept together with glue (I hope that's glue) and there's writing on it, in some language.",
+    rating: 3
+  }
+];
+
+/**
+ * Add our product name to the page title
+ * Get our page page title by the id and the query the .name selector
+ * once you have the element you can add the product name to the span.
+ */
+function setPageTitle() {
+  const pageTitle = document.getElementById('page-title');
+  pageTitle.querySelector('.name').innerText = name;
+}
+
+/**
+ * Add our product description to the page.
+ */
+function setPageDescription() {
+  document.querySelector('.description').innerText = description;
+}
+
+/**
+ * I will display all of the reviews in the reviews array
+ */
+function displayReviews() {
+  if ('content' in document.createElement('template')) {
+    reviews.forEach((review) => {
+      displayReview(review);
+    });
+  } else {
+    console.error('Your browser does not support templates');
+  }
+}
+
+/**
+ *
+ * @param {Object} review The review to display
+ */
+function displayReview(review) {
+  const main = document.getElementById('main');
+  const tmpl = document.getElementById('review-template').content.cloneNode(true);
+  tmpl.querySelector('h4').innerText = review.reviewer;
+  tmpl.querySelector('h3').innerText = review.title;
+  tmpl.querySelector('p').innerText = review.review;
+  // there will always be 1 star because it is a part of the template
+  for (let i = 1; i < review.rating; ++i) {
+    const img = tmpl.querySelector('img').cloneNode();
+    tmpl.querySelector('.rating').appendChild(img);
+  }
+  main.appendChild(tmpl);
+}
+
+// LECTURE STARTS HERE ---------------------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', initializePage);
+
+function initializePage() {
+  // set the product reviews page title
+  setPageTitle();
+  // set the product reviews page description
+  setPageDescription();
+  // display all of the product reviews on our page
+  displayReviews();
+
+  // Add a click event listener for the p.description
+  const desc = document.querySelector('p.description');
+  desc.addEventListener('click', (ev) => {
+    toggleDescriptionEdit(ev.target);
+  });
+
+  // Add a listener for when the edit box loses focus
+  const inputDesc = document.getElementById('inputDesc');
+  inputDesc.addEventListener('blur', function(ev) {
+    exitDescriptionEdit(ev.target, false);
+  });
+
+  // Add a listener for the keyboard. If Enter, then save, if esc, then exit without saving
+  inputDesc.addEventListener('keyup', (ev) => {
+    if (ev.key === 'Enter'){
+      exitDescriptionEdit(ev.target, true);
+    }
+    if (ev.key === 'Escape'){
+      exitDescriptionEdit(ev.target, false);
+    }
+    
+  });
+
+  // Show or hide the form when the user clicks
+  const buttonToggle = document.getElementById('btnToggleForm');
+  buttonToggle.addEventListener('click', (ev) => {
+    showHideForm();
+  });
+
+  // Handle Save Review button to save data
+  document.getElementById('btnSaveReview').addEventListener('click', (ev) => {
+    saveReview();
+    ev.preventDefault();
+  });
+
+}
+
+
+/**
+ * Take an event on the description and swap out the description for a text box.
+ *
+ * @param {Event} event the event object
+ */
+function toggleDescriptionEdit(desc) {
+  const textBox = desc.nextElementSibling;
+  textBox.value = description;
+  textBox.classList.remove('d-none');
+  desc.classList.add('d-none');
+  textBox.focus();
+}
+
+/**
+ * Take an event on the text box and set the description to the contents
+ * of the text box and then hide the text box and show the description.
+ *
+ * @param {Event} event the event object
+ * @param {Boolean} save should we save the description text
+ */
+function exitDescriptionEdit(textBox, save) {
+  let desc = textBox.previousElementSibling;
+  if (save) {
+    description = textBox.value;
+    setPageDescription();
+  }
+  textBox.classList.add('d-none');
+  desc.classList.remove('d-none');
+}
+
+/**
+ * I will show / hide the add review form
+ */
+function showHideForm() {
+  const form = document.querySelector('form');
+  const btn = document.getElementById('btnToggleForm');
+
+  if (form.classList.contains('d-none')) {
+    form.classList.remove('d-none');
+    btn.innerText = 'Hide Form';
+    document.getElementById('name').focus();
+  } else {
+    resetFormValues();
+    form.classList.add('d-none');
+    btn.innerText = 'Add Review';
+  }
+}
+
+/**
+ * I will reset all of the values in the form.
+ */
+function resetFormValues() {
+  const form = document.querySelector('form');
+  const inputs = form.querySelectorAll('input');
+  inputs.forEach((input) => {
+    input.value = '';
+  });
+  document.getElementById('rating').value = 1;
+  document.getElementById('review').value = '';
+}
+
+/**
+ * I will save the review that was added using the add review from
+ */
+function saveReview() {
+  // Grab data from the form and add it to a new review
+
+
+
+  const newReview =   {
+    reviewer: document.getElementById('name').value,
+    title: document.getElementById('title').value,
+    review: document.getElementById('review').value,
+    rating: document.getElementById('rating').value
+  }
+
+  // Add the review to the front of the reviews list
+  reviews.unshift(newReview);
+  displayReview(newReview);
+  showHideForm();
+
+
+ }

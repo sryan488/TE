9e1200ec05bea56e341511b46ded82736e60fda5
Mike Morel <michael.morel@techelevator.com>
2019-11-22 12:50:27 -0500

    M4D5 lecture

---
+++ module-4/05_Fetch_and_Promises/lecture-final/product-reviews/assets/js/reviews.js
@@ -0,0 +1,78 @@
+// An empty reviews array that we will load data into
+let reviews = [];
+
+/******************
+ * TODO: Create a loadReviews function that will fetch reviews from a server, and load them into the reviews array
+ */
+/* Data can be found at:
+  data.json
+  https://my-json-server.typicode.com/blazebiz/json1/reviews
+*/
+function loadReviews() {
+  console.log("Load Reviews...");
+  //const urlToFetch = 'data.json';
+  const urlToFetch = 'https://my-json-server.typicode.com/blazebiz/json1/reviews';
+
+  // TODO: fetch data here...
+
+  fetch(urlToFetch).then(
+    (resp) => {
+      if (resp.ok) {
+        resp.json().then(
+          (data) => {
+            reviews = data;
+            displayReviews();
+          }
+        )
+      } else {
+        console.log(`Error: ${resp.status} ${resp.statusText}`)
+      }
+    }
+  ).catch(
+    (err) => {
+      console.log(err);
+    }
+  )
+
+}
+
+
+/****************
+ * TODO: Once we have a DOM, add an event listener for the button. It will call loadReviews, and then disable itself.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('loadReviews');
+  button.addEventListener('click', (ev) => {
+    loadReviews();
+    //ev.target.disabled = true;
+    ev.target.classList.add('hidden');
+  })
+});
+
+
+/**
+ * This function when invoked will look at an array of reviews
+ * and add it to the page by cloning the #review-template
+ */
+function displayReviews() {
+  console.log("Display Reviews...");
+
+  // first check to make sure the browser supports content templates
+  if ('content' in document.createElement('template')) {
+    // query the document for .reviews and assign it to a variable called container
+    const container = document.querySelector(".reviews");
+    // loop over the reviews array
+    reviews.forEach((review) => {
+      // get the template; find all the elements and add the data from our review to each element
+      const tmpl = document.getElementById('review-template').content.cloneNode(true);
+      tmpl.querySelector('img').setAttribute("src", review.avatar);
+      tmpl.querySelector('.username').innerText = review.username;
+      tmpl.querySelector('h2').innerText = review.title;
+      tmpl.querySelector('.published-date').innerText = review.publishedOn;
+      tmpl.querySelector('.user-review').innerText = review.review;
+      container.appendChild(tmpl);
+    });
+  } else {
+    console.error('Your browser does not support templates');
+  }
+}

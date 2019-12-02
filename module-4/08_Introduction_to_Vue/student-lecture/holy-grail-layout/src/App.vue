221b8e39602ca5d4202fec48dcd1daba4a83cc35
David Wintrich <david@techelevator.com>
2019-12-02 08:16:49 -0500

    M4D8 initial lecture code

---
+++ module-4/08_Introduction_to_Vue/student-lecture/holy-grail-layout/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <header>Header</header>
+    <aside class="sidebar-left">Left Sidebar</aside>
+    <article>Article</article>
+    <aside class="sidebar-right">Right Sidebar</aside>
+    <footer>Footer</footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {}
+};
+</script>
+
+<style>
+body {
+  margin: 0px;
+  padding: 0px;
+}
+#app {
+  display: grid;
+  grid-template-columns: 150px auto 150px;
+  grid-template-rows: 60px 1fr 100px;
+  grid-gap: 1em;
+  height: 100vh;
+}
+
+header,
+aside,
+article,
+footer {
+  background: #eaeaea;
+  padding: 1em;
+  color: #949494;
+}
+
+header,
+footer {
+  grid-column: 1 / 4;
+}
+</style>
